a1da3b6b83122979d00d4592d56ba2d175e0b93b
JanzaibYousaf <154719041+janzaibyousaf@users.noreply.github.com>
2024-05-13 12:26:50 +0500

    Add files via upload

---
+++ 20-ForLoopNested/app.js
@@ -0,0 +1,25 @@
+for (var a = 1; a <=5 ;a++) {
+for (var b = 1; b <=a; b++ ){
+    document.write(b ,"  ")
+}
+
+document.write(" <br>")
+    
+}
+
+for (var a = 5; a >= 1; a--){
+
+    for (var b = a;  b >=1; b--  ){
+        document.write(b)
+    }
+    document.write("<br>")
+}
+
+
+for (var a = 5; a >= 1; a--){
+
+    for (var b = 1;  b <=a; b++  ){
+        document.write(a)
+    }
+    document.write("<br>")
+}
